4fba2e31ab4791439611c61c96629beca89fca84
silentlyexisting <silentlyexistin@gmail.com>
2021-12-27 17:30:03 +0300

    deploy

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,7 +1,19 @@
 package hexlet.code;
 
+import io.javalin.Javalin;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Javalin app = getApp();
+        app.start();
+    }
+
+    public static Javalin getApp() {
+        Javalin app = Javalin.create(javalinConfig -> {
+            javalinConfig.enableDevLogging();
+        });
+
+        app.get("/", ctx -> ctx.result("Hello World!"));
+        return app;
     }
 }
