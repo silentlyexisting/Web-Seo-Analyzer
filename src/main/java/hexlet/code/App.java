07cba6e15629ca0c1180bc83b3b546daedc70734
silentlyexisting <silentlyexistin@gmail.com>
2021-12-30 01:26:33 +0300

    added rootcontroller (main page)

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,6 +1,12 @@
 package hexlet.code;
 
+import hexlet.code.controllers.RootController;
 import io.javalin.Javalin;
+import io.javalin.plugin.rendering.template.JavalinThymeleaf;
+import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
 public class App {
     public static void main(String[] args) {
@@ -11,9 +17,21 @@ public class App {
     public static Javalin getApp() {
         Javalin app = Javalin.create(javalinConfig -> {
             javalinConfig.enableDevLogging();
+
+            JavalinThymeleaf.configure(getTemplateEngine());
         });
 
-        app.get("/", ctx -> ctx.result("Hello World!"));
+        app.get("/", RootController.welcome);
         return app;
+    }
+
+    private static TemplateEngine getTemplateEngine() {
+        TemplateEngine templateEngine = new TemplateEngine();
+        templateEngine.addDialect(new LayoutDialect());
+        templateEngine.addDialect(new Java8TimeDialect());
+        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+        templateResolver.setPrefix("/templates/");
+        templateEngine.addTemplateResolver(templateResolver);
+        return templateEngine;
     }
 }
--- src/main/java/hexlet/code/Url.java
+++ src/main/java/hexlet/code/Url.java
@@ -1,15 +1,17 @@
 package hexlet.code;
 
 import io.ebean.Model;
-import io.ebean.config.JsonConfig;
+import io.ebean.annotation.WhenCreated;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import java.time.Instant;
 
 @Entity
 public class Url extends Model {
     @Id
     private long id;
     private String name;
-    private JsonConfig.DateTime createdAt;
+    @WhenCreated
+    private Instant createdAt;
 }
